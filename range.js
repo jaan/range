cb88f53899af19db2ad09eee07875a526a7568aa
Janardhan <njraju@gmail.com>
2014-08-06 22:36:13 -0700

    Create range.js

---
+++ range.js
@@ -0,0 +1,24 @@
+  (function(window, undefined) {
+  'use strict';
+    var range;
+     range= {
+      logarithmicScale: function (num,sep){
+         var digits = this.getDigits(num);
+         var sep = sep?sep: "-";
+
+         return (Math.pow(10,digits-1) +''+ sep +''+ Math.pow(10,digits));
+      },
+      naturalSteps: function (num, sep){
+
+      },
+      getDigits : function(num){
+        //get the digits, similar to Math.floor
+        var digits = ~~(Math.log(number) / Math.LN10 + 1);
+        return digits;
+      }
+
+     };
+    
+      // export to global namespace
+      window.range = range;
+   })(window);
